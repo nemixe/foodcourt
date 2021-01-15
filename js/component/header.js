ce9f8aebfe567ff52f636864fbeecf6b25c7fe16
nemixe <rafel.ami@gmail.com>
2021-01-15 13:28:42 +0700

    improve accessibility and seo

--- js/component/header.js
+++ js/component/header.js
@@ -1,7 +1,7 @@
 class Header extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
-        <div class="absolute text-3xl font-bold my-6 lg:my-12 select-none px-2">food court</div>
+        <header class="absolute text-3xl font-bold my-6 lg:my-12 select-none px-2">food court</header>
     `;
     }
 }
